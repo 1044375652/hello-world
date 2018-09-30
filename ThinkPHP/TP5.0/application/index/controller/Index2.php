726b8f0f6bc75c7bd72809de411e2955ee72b7d0
1044375652 <1044375652@qq.com>
2018-09-30 15:42:53 +0800

    test

---
+++ ThinkPHP/TP5.0/application/index/controller/Index2.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+use think\Db;
+use think\Url;
+use app\index\model\Users;
+use app\index\model\UserLevel;
+use app\index\model\Test;
+class Index2 extends Controller
+{
+   function index2(){
+       echo 'I am index2';
+   }
+}
---
+++ ThinkPHP/TP5.0/runtime/temp/05d2e0aad051e0a6ddb8b6e05c7237d6.php
@@ -0,0 +1,9 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:79:"D:\wamp64\www\ThinkPHP\TP5.0\public/../application/admin\view\index\index2.html";i:1475143277;}*/ ?>
+<html>
+<head>
+<title>hello2 <?php echo $name; ?></title>
+</head>
+<body>
+    hello2, <?php echo $name; ?>!
+</body>
+</html>
